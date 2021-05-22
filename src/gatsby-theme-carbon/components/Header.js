ca7d4ca264c3a960ef5ce64d5d49d4b841674014
James Dow <james.dow@us.ibm.com>
2021-05-22 11:00:05 -0500

    first commit

---
+++ src/gatsby-theme-carbon/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Header from 'gatsby-theme-carbon/src/components/Header';
+
+const CustomHeader = (props) => (
+  <Header {...props}>
+    Jenny Hsiao Sanchez
+  </Header>
+);
+
+export default CustomHeader;
