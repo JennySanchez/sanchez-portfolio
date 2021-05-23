871464ebc1261b6cbbb9ed6c004920421a892c0b
James Dow <james.dow@us.ibm.com>
2021-05-23 00:46:45 -0500

    favicon, tabs, contact form

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,21 +3,28 @@ module.exports = {
   siteMetadata: {
     title: "Jenny Hsiao Sanchez",
     description: "Jenny Hsiao Sanchez's portfolio of work as a maker, design thinker, and a professional multidisciplinary Designer.",
-    keywords: "gatsby,theme,carbon",
+    keywords: "Jenny Sanchez, Portfolio, Design, Enterprise Design Thinking",
+    lang: "en",
   },
   plugins: [
     {
       resolve: "gatsby-plugin-manifest",
       options: {
-        name: "Carbon Design Gatsby Theme",
-        icon: "src/images/favicon.svg",
-        short_name: "Gatsby Theme Carbon",
+        name: "Jenny Hsiao Sanchez Portfolio",
+        icon: "src/images/sanchez-favicon.svg",
+        short_name: "Jenny Hsiao Sanchez",
         start_url: "/",
         background_color: "#ffffff",
         theme_color: "#0062ff",
         display: "browser",
       },
     },
-    'gatsby-theme-carbon'
+    {
+        resolve: "gatsby-theme-carbon",
+        options: {
+            iconPath: "src/images/sanchez-favicon.svg",
+            titleType: "prepend",
+        }
+    }
   ],
 };
