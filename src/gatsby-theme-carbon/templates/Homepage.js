b97c60b0942d5936e0a003740e68bd3a27dfb1f1
James Dow <james.dow@us.ibm.com>
2021-05-24 02:14:15 -0500

    added homepage leadspace stuff

--- src/gatsby-theme-carbon/templates/Homepage.js
+++ src/gatsby-theme-carbon/templates/Homepage.js
@@ -1,62 +1,48 @@
 import React from 'react';
-import { HomepageBanner, HomepageCallout } from 'gatsby-theme-carbon';
+import { HomepageBanner, HomepageCallout, ImageCard, Row, Column } from 'gatsby-theme-carbon';
 import HomepageTemplate from 'gatsby-theme-carbon/src/templates/Homepage';
 import { calloutLink } from './Homepage.module.scss';
 
 import Carbon from '../../images/carbon.jpg';
 
-const FirstLeftText = () => <p>Callout component</p>;
-
-const FirstRightText = () => (
-  <p>
-    This is a callout component. You can edit the contents by updating the{' '}
-    <a href="https://github.com/carbon-design-system/gatsby-theme-carbon/blob/5fe12de31bb19fbfa2cab7c69cd942f55aa06f79/packages/example/src/gatsby-theme-carbon/templates/Homepage.js">
-      pre-shadowed homepage template
-    </a>
-    . You can also provide <code>color</code> and <code>backgroundColor</code>{' '}
-    props to suit your theme.
-    <a
-      className={calloutLink}
-      href="https://github.com/carbon-design-system/gatsby-theme-carbon/blob/main/packages/example/src/gatsby-theme-carbon/templates/Homepage.js">
-      Homepage source →
-    </a>
-  </p>
-);
-
-const SecondLeftText = () => <p>Callout component</p>;
-
-const SecondRightText = () => (
-  <p>
-    You can also not use these components at all by not providing the callout
-    props to the template or writing your own template.
-    <a
-      className={calloutLink}
-      href="https://github.com/carbon-design-system/gatsby-theme-carbon/blob/main/packages/example/src/gatsby-theme-carbon/templates/Homepage.js">
-      Homepage source →
-    </a>
-  </p>
-);
-
-const BannerText = () => <h1>Carbon Gatsby theme</h1>;
+function Leadspace () {
+    return (
+        <div class="leadspace">
+            <div className="bx--grid">
+                <Row className="image-card-group">
+                  <Column colMd={4} colLg={4} noGutterSm />
+                  <Column colMd={4} colLg={4} noGutterSm>
+                    <ImageCard
+                      title="Work"
+                      aspectRatio="1:2"
+                      href="/work/case-study"
+                      className="leadspace__cta"
+                      actionIcon="arrowRight" />
+                  </Column>
+                  <Column colMd={4} colLg={4} noGutterSm>
+                    <ImageCard
+                        title="Info"
+                        aspectRatio="1:1"
+                        href="/info/about-me"
+                        className="leadspace__cta"
+                        actionIcon="arrowRight" />
+                    <ImageCard
+                        title="Contact"
+                        aspectRatio="1:1"
+                        actionIcon="arrowRight"
+                        className="leadspace__cta"
+                        href="/contact" />
+                  </Column>
+                </Row>
+            </div>
+        </div>
+    );
+}
 
 const customProps = {
-  Banner: <HomepageBanner renderText={BannerText} image={Carbon} />,
-  FirstCallout: (
-    <HomepageCallout
-      backgroundColor="#030303"
-      color="white"
-      leftText={FirstLeftText}
-      rightText={FirstRightText}
-    />
-  ),
-  SecondCallout: (
-    <HomepageCallout
-      leftText={SecondLeftText}
-      rightText={SecondRightText}
-      color="white"
-      backgroundColor="#061f80"
-    />
-  ),
+  Banner: <Leadspace />,
+  FirstCallout: null,
+  SecondCallout: null,
 };
 
 // spreading the original props gives us props.children (mdx content)
